83dbaa34eb48d21ed8a7799550a090abbdb0b7ed
anyou4432 <kyoandya@gmail.com>
2022-12-28 00:43:00 +0900

    update

--- app.py
+++ app.py
@@ -75,7 +75,7 @@ def index():
 
 @app.after_request
 def after_request(response):
-  # response.headers.add('Access-Control-Allow-Origin', '*')
+  response.headers.add('Access-Control-Allow-Origin', '*')
   response.headers.add('Access-Control-Allow-Headers', 'Content-Type')
   response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
   return response
