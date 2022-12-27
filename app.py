be2fa109310230d1b05f19fa5aeb285ec8314708
anyou4432 <kyoandya@gmail.com>
2022-12-27 23:43:45 +0900

    edit app.py

--- app.py
+++ app.py
@@ -51,19 +51,16 @@ if os.path.isfile(mapping_file_path):
         img_class_map = json.load(f)
 
 # Transform input into the form our model expects
-def transform_image(infile):
-    input_transforms = [transforms.Resize(255),           # We use multiple TorchVision transforms to ready the image
-        transforms.CenterCrop(224),
-        transforms.ToTensor(),
-        transforms.Normalize([0.485, 0.456, 0.406],       # Standard normalization for ImageNet model input
-            [0.229, 0.224, 0.225])]
-    my_transforms = transforms.Compose(input_transforms)
-    image = Image.open(infile)                            # Open the image file
-    timg = my_transforms(image)                           # Transform PIL image to appropriately-shaped PyTorch tensor
-    timg.unsqueeze_(0)                                    # PyTorch models expect batched input; create a batch of 1
-    return timg
+def transform_image(image_bytes):
+    my_transforms = transforms.Compose([transforms.Resize(255),
+                                        transforms.CenterCrop(224),
+                                        transforms.ToTensor(),
+                                        transforms.Normalize(
+                                            [0.485, 0.456, 0.406],
+                                            [0.229, 0.224, 0.225])])
+    image = Image.open(io.BytesIO(image_bytes))
+    return my_transforms(image).unsqueeze(0)
 
-# Get a prediction
 def get_prediction(image_bytes):
     tensor = transform_image(image_bytes=image_bytes)
     outputs = model.forward(tensor)
@@ -87,10 +84,10 @@ def after_request(response):
 def predict():
     if request.method == 'POST':
         file = request.files['file']
-        if file is not None:
-            input_tensor = transform_image(file)
-            class_name  = get_prediction(input_tensor)
-            return jsonify({'class_name': class_name})
+        img_bytes = file.read()
+        #class_id, class_name = get_prediction(image_bytes=img_bytes)
+        class_name = get_prediction(image_bytes=img_bytes)
+        return jsonify({'class_name': class_name})
 
 #LINE BOTウェブフック
 @app.route("/callback", methods=['POST'])
